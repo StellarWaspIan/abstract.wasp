1fe85e22d94b4e930ea31d4c70d932f8185905c2
StellarWasp <avilaian2officialac@gmail.com>
2025-10-15 09:25:58 -0300

    punto2 terminado

--- tp3so/punto2.c
+++ tp3so/punto2.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(){
 	char cadena1[30];
     char cadena2[30];
 	printf("Ingrese la palabra 1: ");
 	scanf("%s", cadena1);
-	printf("Ingrese el valor 2: ");
+	printf("Ingrese el palabra 2: ");
 	scanf("%s", cadena2);
 	
-	printf("Valor 1: %s",cadena1);
+	qsort(cadena1);
+	qsort(cadena2);
+
+	printf("Palabras Ordenadas: ");
     printf("\n");
-	printf("Valor 2: %s",cadena2);
+	printf("Palabra 1: %s",cadena1);
     printf("\n");
+	printf("Palabra 2: %s",cadena2);
+    printf("\n");
+	
 	return 0;
 }
