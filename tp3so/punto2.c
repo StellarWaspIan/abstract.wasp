02053f1ba5e820e27cae2f5c5a869b23816ef0a3
StellarWasp <avilaian2officialac@gmail.com>
2025-10-15 09:30:08 -0300

    punto2 terminado

--- tp3so/punto2.c
+++ tp3so/punto2.c
@@ -3,6 +3,11 @@
 #include <string.h>
 
 int main(){
+
+	//funcion de comparacion para el ordenamiento
+	int compararChar(const void *a, const void *b) {
+    return (*(char *)a - *(char *)b);
+	}
 	char cadena1[30];
     char cadena2[30];
 	printf("Ingrese la palabra 1: ");
@@ -10,8 +15,8 @@ int main(){
 	printf("Ingrese el palabra 2: ");
 	scanf("%s", cadena2);
 	
-	qsort(cadena1);
-	qsort(cadena2);
+	qsort(cadena1, strlen(cadena1), sizeof(char), compararChar);
+	qsort(cadena2, strlen(cadena2), sizeof(char), compararChar);
 
 	printf("Palabras Ordenadas: ");
     printf("\n");
